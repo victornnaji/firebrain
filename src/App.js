5b79a772c1c760a0c748021447263d023bfd61ca
Nnaji Victor <nnajivictor0@gmail.com>
2019-10-25 17:11:52 +0100

    starter files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+        <Navigation />
+        
+      </div>
     </div>
   );
 }
